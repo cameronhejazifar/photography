918ebb943e79f2735c7b2309174563cfedd2393b
Cameron Hejazi <cameron@hejazi-far.com>
2020-10-06 08:02:40 -0600

    added tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [
+      './resources/**/*.php',
+      './resources/**/*.html',
+      './resources/**/*.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
--- webpack.mix.js
+++ webpack.mix.js
@@ -14,5 +14,5 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .copyDirectory('resources/img', 'public/img')
     .postCss('resources/css/app.css', 'public/css', [
-        //
+        require('tailwindcss'),
     ]);
