f9976fabc892e67e8904e9da6b69e0aad9931497
Cameron Hejazi <cameron@hejazi-far.com>
2020-10-06 09:28:31 -0600

    created landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,30 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: [
-      './resources/**/*.php',
-      './resources/**/*.html',
-      './resources/**/*.vue',
-  ],
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
+    future: {
+        // removeDeprecatedGapUtilities: true,
+        // purgeLayersByDefault: true,
+    },
+    purge: [
+        './resources/**/*.php',
+        './resources/**/*.html',
+        './resources/**/*.vue',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                'lato': ['Lato', 'sans-serif'],
+                'signerica': ['Signerica', 'sans-serif'],
+            },
+            fontSize: {
+                logo: '8rem',
+            },
+            letterSpacing: {
+                logo: '1rem',
+            },
+            zIndex: {
+                behind: '-1',
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
 };
