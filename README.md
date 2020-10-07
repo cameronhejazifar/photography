39cd37e1b6efe3f116fb13f09c5c45ef58f456ec
Cameron Hejazi <cameron@hejazi-far.com>
2020-10-07 09:45:17 -0600

    added create-user command

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
+# Photography
+
+## Commands
+
+### Create User
+`php artisan create-user`
+This command allows you to manually create a user in the database. You will be prompted for the email, password, and name of the user to be created. Email addresses must be unique among users.
+
+
+
+
 <p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400"></a></p>
 
 <p align="center">
---
+++ app/Console/Commands/CreateUser.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Hash;
+use Illuminate\Console\Command;
+use Illuminate\Validation\ValidationException;
+use Validator;
+
+class CreateUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'create-user';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Creates a user';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     * @throws \Throwable
+     */
+    public function handle()
+    {
+        try {
+            $data = Validator::validate([
+                'email' => $this->ask('Enter Email'),
+                'password' => $this->ask('Enter Password'),
+                'name' => $this->ask('Enter Full Name'),
+            ], [
+                'email' => 'required|email|unique:users',
+                'password' => 'required|string|between:5,100',
+                'name' => 'required|string|between:2,255',
+            ]);
+        } catch (ValidationException $e) {
+            $this->error($e->getMessage());
+            foreach ($e->validator->getMessageBag()->all() as $errorMessage) {
+                $this->error("  - {$errorMessage}");
+            }
+            return 1;
+        }
+
+        $user = new User;
+        $user->email = $data['email'];
+        $user->password = Hash::make($data['password']);
+        $user->name = $data['name'];
+        $user->email_verified_at = Carbon::now();
+        $user->saveOrFail();
+        $this->info("User created: ID = {$user->id}");
+
+        return 0;
+    }
+}
