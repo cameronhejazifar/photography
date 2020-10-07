fff1f3642cb0a32f710d3d930d4655c3f721c135
Cameron Hejazi <cameron@hejazi-far.com>
2020-10-07 12:46:43 -0600

    added profile information to db

---
+++ database/migrations/2020_10_07_183439_add_users_profile_columns.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddUsersProfileColumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('alias')->nullable()->after('remember_token');
+            $table->date('date_of_birth')->nullable()->after('alias');
+            $table->text('biography')->nullable()->after('date_of_birth');
+            $table->text('photograph_checklist')->nullable()->after('biography');
+            $table->boolean('active')->default(1)->after('photograph_checklist');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('alias');
+            $table->dropColumn('date_of_birth');
+            $table->dropColumn('biography');
+            $table->dropColumn('photograph_checklist');
+            $table->dropColumn('active');
+        });
+    }
+}
